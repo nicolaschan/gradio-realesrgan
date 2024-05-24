84c5f345c3437cf0ec3387013e3295fc58bff42a
Nicolas Chan <nicolas@nicolaschan.com>
2024-05-24 14:02:37 -0700

    Add more to build

--- build.sh
+++ build.sh
@@ -2,3 +2,6 @@
 
 wget https://github.com/xinntao/Real-ESRGAN/releases/download/v0.1.0/RealESRGAN_x4plus.pth -P weights
 podman build -t gradio-realesrgan .
+
+# Then run like:
+# podman run -p 10003:7860 --gpus all -d localhost/gradio-realesrgan
